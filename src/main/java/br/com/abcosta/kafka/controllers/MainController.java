70ef3167a51dce766aec666dcfaeecac7e1929cb
ab-costa <costa.alessandroborges@gmail.com>
2022-05-02 17:53:54 -0300

    Feature: Ciação do package aws, da classe S3Util e do endpoint /upload

--- src/main/java/br/com/abcosta/kafka/controllers/MainController.java
+++ src/main/java/br/com/abcosta/kafka/controllers/MainController.java
@@ -1,7 +1,13 @@
 package br.com.abcosta.kafka.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import br.com.abcosta.kafka.aws.S3Util;
 
 @RestController
 public class MainController {
@@ -9,5 +15,27 @@ public class MainController {
     @GetMapping("")
     public String viewHomePage() {
         return "upload";
+    }
+
+    @PostMapping("/upload")
+    public String handleUploadForm(Model model, String description,
+            @RequestParam("file") MultipartFile multipart) {
+        String fileName = multipart.getOriginalFilename();
+         
+        System.out.println("Description: " + description);
+        System.out.println("filename: " + fileName);
+         
+        String message = "";
+         
+        try {
+            S3Util.uploadFile(fileName, multipart.getInputStream());
+            message = "Your file has been uploaded successfully!";
+        } catch (Exception ex) {
+            message = "Error uploading file: " + ex.getMessage();
+        }
+         
+        model.addAttribute("message", message);
+         
+        return "message";              
     }
 }
