660bf4e8a6aa5ab49cd625b2a70d909f2449e2fc
ab-costa <costa.alessandroborges@gmail.com>
2022-05-02 22:59:07 -0300

    Feature: Criação da classe S3Download, do endpoint /download e do template download

---
+++ src/main/java/br/com/abcosta/kafka/aws/S3DownloadFile.java
@@ -0,0 +1,39 @@
+package br.com.abcosta.kafka.aws;
+
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
+
+public class S3DownloadFile {
+
+    public static void downloadFile(String fileName) throws IOException {
+        String bucket = "abcosta-bucket";
+        String key = fileName;
+         
+        S3Client client = S3Client.builder().build();
+         
+        GetObjectRequest request = GetObjectRequest.builder()
+                        .bucket(bucket)
+                        .key(key)
+                        .build();
+         
+        ResponseInputStream<GetObjectResponse> response = client.getObject(request);
+                 
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(key));
+         
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+         
+        while ((bytesRead = response.read(buffer)) !=  -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+                             
+        response.close();
+        outputStream.close();
+    }
+}
--- src/main/java/br/com/abcosta/kafka/controllers/MainController.java
+++ src/main/java/br/com/abcosta/kafka/controllers/MainController.java
@@ -1,13 +1,22 @@
 package br.com.abcosta.kafka.controllers;
 
+import java.io.BufferedOutputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
 import br.com.abcosta.kafka.aws.S3UploadFile;
+import software.amazon.awssdk.core.ResponseInputStream;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectResponse;
 
 @Controller
 public class MainController {
@@ -37,5 +46,41 @@ public class MainController {
         model.addAttribute("message", message);
          
         return "message";              
+    }
+
+    @GetMapping("/download/{fileName}")
+    public String downloadFile(@PathVariable String fileName, Model model) throws IOException {
+        System.out.println(">>>>> DOWNLOAD FILE");
+        System.out.println(fileName);
+
+        String bucket = "abcosta-bucket";
+        String key = fileName;
+
+        S3Client client = S3Client.builder().build();
+
+        GetObjectRequest request = GetObjectRequest.builder()
+                                                   .bucket(bucket)
+                                                   .key(key)
+                                                   .build();
+
+        ResponseInputStream<GetObjectResponse> response = client.getObject(request);
+
+        BufferedOutputStream outputStream = new BufferedOutputStream(new FileOutputStream(key));
+
+        byte[] buffer = new byte[4096];
+        int bytesRead = -1;
+         
+        while ((bytesRead = response.read(buffer)) !=  -1) {
+            outputStream.write(buffer, 0, bytesRead);
+        }
+                             
+        response.close();
+        outputStream.close();
+
+        System.out.println(">>>>> ANTES DE RENDERIZAR A PÁGINA DOWNLOAD");
+
+        model.addAttribute("download", fileName);
+
+        return "download";
     }
 }
